bcd3cb0ee25e6cedc9ff115e1d304f783b4a8976
住港伴Dev <dev@zhugangban.com>
2026-05-17 20:55:26 +0000

    fix: 学校页改为原生WXML渲染 + 链接移至关卡5
    - web-view方案因测试域名限制不可用,改为corpus.json原生渲染
    - 搜索+类型筛选(官立/资助/直资/私立/国际)
    - 150所学校卡片式列表
    - 学校链接移至关卡5内部(校网向导下方)

--- pages/schools/index.js
+++ pages/schools/index.js
@@ -1,4 +1,53 @@
+var corpus = require('../../data/school-corpus.json');
+
 Page({
-  data: { url: 'https://cloudbase-d1g17tgt7cc199a60-1429050918.tcloudbaseapp.com/schools/index.html' },
-  onLoad: function() {}
+  data: {
+    schools: [],
+    filtered: [],
+    keyword: '',
+    activeType: '全部'
+  },
+
+  onLoad: function() {
+    var all = [];
+    for (var key in corpus) {
+      var s = corpus[key];
+      all.push({
+        id: s.id || key,
+        name: s['学校名称'] || '',
+        type: s['类别'] || '',
+        net: s['所属校网'] || '',
+        region: s['地区'] || '',
+        fee: s['学费/年'] || '',
+        difficulty: s['插班难度'] || '',
+        curriculum: s['课程体系'] || ''
+      });
+    }
+    this.setData({ schools: all });
+    this.doFilter();
+  },
+
+  onSearch: function(e) {
+    this.setData({ keyword: e.detail.value });
+    this.doFilter();
+  },
+
+  onFilter: function(e) {
+    this.setData({ activeType: e.currentTarget.dataset.type });
+    this.doFilter();
+  },
+
+  doFilter: function() {
+    var type = this.data.activeType;
+    var kw = (this.data.keyword || '').toLowerCase();
+    var filtered = this.data.schools.filter(function(s) {
+      if (type !== '全部' && s.type !== type) return false;
+      if (kw) {
+        var txt = (s.name + s.region + s.net + s.curriculum).toLowerCase();
+        if (txt.indexOf(kw) < 0) return false;
+      }
+      return true;
+    });
+    this.setData({ filtered: filtered });
+  }
 });
